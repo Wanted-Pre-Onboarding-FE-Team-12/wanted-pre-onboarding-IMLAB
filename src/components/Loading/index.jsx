3d4b4b5b7cfbc16194170a0d043504da2cc4c043
bbbb <bgh9651@gmail.com>
2022-09-07 00:06:22 +0900

    assets 폴더 loading image add

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import loading from '../../assets/loading.gif';
+import loadingdark from '../../assets/loadingdark.gif';
 import * as S from './style';
+import Layout from '../../layout/index';
 
 const Loading = () => {
   return (
-    <S.Background>
-      <S.LoadingText>잠시만 기다려 주세요.</S.LoadingText>
-      <img src={loading} alt="로딩" />
-    </S.Background>
+    <Layout>
+      <S.Background>
+        <S.LoadingText>잠시만 기다려 주세요.</S.LoadingText>
+        <img src={loadingdark} alt="로딩" />
+      </S.Background>
+    </Layout>
   );
 };
 

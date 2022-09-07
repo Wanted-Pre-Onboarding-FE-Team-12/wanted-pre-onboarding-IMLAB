54aa31ac10792d788ecdf4419adc622a3b24b788
Jaehoon Kim <evolui@naver.com>
2022-09-07 22:12:03 +0900

    fix: infinite query fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
 

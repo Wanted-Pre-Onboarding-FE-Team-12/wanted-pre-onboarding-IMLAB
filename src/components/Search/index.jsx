72972a142e2e05021f6ff3f1bd2d44dd3cfd0f34
jiye-7 <jy522hi@gmail.com>
2022-09-07 18:05:04 +0900

    feature: search page
    - search page layout
    - axios module 미적용 (main 머지 후 적용 예정)

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,15 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBox = () => {
+  const navigate = useNavigate();
+  const [searchWord, setSearchWord] = useState('');
+
+  const handleToSearchSubmit = e => {
+    e.preventDefault();
+    navigate('/search', { state: { searchWord } });
+  };
+
+  const handleChange = ({ target }) => {
+    setSearchWord('');
+    const word = target.value;
+    if (word) {
+      setSearchWord(word);
+    }
+  };
+
   return (
-    <S.SearchBoxContainer>
-      <input className="search-text" type="text" name="" />
-      <Link to="/search" className="search-btn">
+    <S.SearchBoxContainer onSubmit={handleToSearchSubmit}>
+      <input value={searchWord ?? ''} className="search-text" type="text" onChange={handleChange} />
+      <button className="search-btn">
         <FontAwesomeIcon icon={faSearch} />
-      </Link>
+      </button>
     </S.SearchBoxContainer>
   );
 };

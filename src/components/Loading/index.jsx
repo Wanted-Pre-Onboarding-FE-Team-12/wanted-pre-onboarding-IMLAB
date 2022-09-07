ac87e874a687d635b6e638385b05f3eb024fa584
bbbb <bgh9651@gmail.com>
2022-09-07 10:27:39 +0900

    laoding gif에서 라이브러리로 변경

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import loadingdark from '../../assets/loadingdark.gif';
 import * as S from './style';
 import Layout from '../../layout/index';
+import { Oval } from 'react-loader-spinner';
 
 const Loading = () => {
   return (
     <Layout>
       <S.Background>
-        <S.LoadingText>잠시만 기다려 주세요.</S.LoadingText>
-        <img src={loadingdark} alt="로딩" />
+        {/* <S.LoadingText>잠시만 기다려 주세요.</S.LoadingText>
+        <img src={loadingdark} alt="로딩" /> */}
+        <Oval
+          height={80}
+          width={80}
+          color="white"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor="#4fa94d"
+          strokeWidth={2}
+          strokeWidthSecondary={2}
+        />
       </S.Background>
     </Layout>
   );

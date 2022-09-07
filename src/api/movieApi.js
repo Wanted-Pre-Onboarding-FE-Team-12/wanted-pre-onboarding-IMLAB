41a6b8612f3989329b5ada26616ce41fb47c63af
bbbb <bgh9651@gmail.com>
2022-09-07 23:46:52 +0900

    rebase 충돌 해결

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -1,11 +1,7 @@
 import { instance } from './index';
 
-export const getPopularMovie = async page => {
-<<<<<<< HEAD
+export const getPopularMovie = async page => {  
   const response = await instance.get('movie/popular', { params: { page } });
-=======
-  const response = await instance.get('popular', { params: { page } });
->>>>>>> 7e55434 (card 컴포넌트 레이아웃 구현 및 star rating 구현)
   return response.data;
 };
 
@@ -19,14 +15,11 @@ export const getUpcomingMovie = async page => {
   return response.data;
 };
 
-<<<<<<< HEAD
-=======
 export const getTopRatedMovie = async page => {
   const response = await instance.get('movie/top_rated', { params: { page } });
   return response.data;
 };
 
->>>>>>> 7e55434 (card 컴포넌트 레이아웃 구현 및 star rating 구현)
 export const getMovieDetail = async id => {
   const response = await instance.get(`movie/${id}`);
   return response.data;
@@ -36,7 +29,6 @@ export const searchMovie = async (query, page) => {
   const response = await instance.get(`search/movie`, { params: { query, page } });
   return response.data;
 };
-<<<<<<< HEAD
 
 export const getTopRated = async ({ pageParam = 1 }) => {
   const { data } = await instance.get(`movie/top_rated?page=${pageParam}`);
@@ -48,5 +40,3 @@ export const getTopRated = async ({ pageParam = 1 }) => {
     isLast,
   };
 };
-=======
->>>>>>> 7e55434 (card 컴포넌트 레이아웃 구현 및 star rating 구현)
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 
-<<<<<<< HEAD
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -26,15 +24,4 @@ root.render(
       <App />
     </BrowserRouter>
   </QueryClientProvider>,
-=======
-const queryClient = new QueryClient();
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </BrowserRouter>,
->>>>>>> 827917b (대체 이미지 적용 및 query 구현중)
 );

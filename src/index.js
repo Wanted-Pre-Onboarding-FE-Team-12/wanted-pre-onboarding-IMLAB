21ec93ea21135af1e18a5549ed6d9537e940182f
bbbb <bgh9651@gmail.com>
2022-09-07 10:20:01 +0900

    card 컴포넌트 레이아웃 구현 및 star rating, infinite scroll, 대체이미지 구현

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 
+<<<<<<< HEAD
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -25,4 +26,15 @@ root.render(
       <App />
     </BrowserRouter>
   </QueryClientProvider>,
+=======
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>,
+>>>>>>> 827917b (대체 이미지 적용 및 query 구현중)
 );

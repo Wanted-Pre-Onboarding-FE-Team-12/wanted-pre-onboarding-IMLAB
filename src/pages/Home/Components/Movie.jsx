00b8fda05bb756bc71468d14e118973020243eff
wooyoon <dndbs14@gmail.com>
2022-09-07 13:07:57 +0900

    feat: Home 레이아웃
    받아온 Popular Movies 데이터 리스트를 마크업했습니다.

---
+++ src/pages/Home/Components/Movie.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Movie = ({ movie }) => {
+    const {id, poster_path, title} = movie;
+    const prefix = "https://image.tmdb.org/t/p/w500"
+    
+    return (
+        <MovieWrapper key={id}>
+            <MoviePoster src={`${prefix}${poster_path}`}></MoviePoster>
+            <MovieDescription>{title}</MovieDescription>
+        </MovieWrapper>
+    );
+};
+
+export default Movie;
+
+const MovieWrapper = styled.div`
+    width: 150px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    margin: 1rem;
+`;
+const MoviePoster = styled.img`
+    width:150px;
+`;
+const MovieDescription = styled.h2`
+`;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Layout from 'layout';
+import { getPopularMovie } from './popularApi';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import Movie from './Components/Movie';
+
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  async function setMoviesList() {
+    const popularMovies = await getPopularMovie();
+    setPopularMovies(popularMovies);
+  }
+
+  useEffect(() => {
+    setMoviesList();
+  }, []);
+
+  function getMainMovie(){
+    if (!popularMovies.length){
+      return ''
+    } else {
+      const prefix = "https://image.tmdb.org/t/p/w500"
+      return prefix+popularMovies[0].backdrop_path
+    }
+  }
+  return <Layout>
+    <HomeWrapper>
+      <MainMovieThumbNail src={getMainMovie()}>
+      </MainMovieThumbNail>
+      <h1>Popular Movies</h1>
+      <PopularMovies>
+        {popularMovies?.map(movie=>(
+          <Movie movie={movie} key={movie.id}/>
+        ))}
+      </PopularMovies>
+    </HomeWrapper>
+  </Layout>;
+};
+
+export default Home;
+
+const HomeWrapper = styled.div`
+  color: white;
+  display:flex;
+  flex-direction:column;
+  width:100%;
+  justify-content: center;
+  align-items:center;
+`;
+const MainMovieThumbNail = styled.img`
+  margin: 3rem 0;
+`;
+const PopularMovies = styled.div`
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+`;
